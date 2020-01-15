9cfaae19f8ffe65474b22582db31e4a43685f9ec
Anand Natarajan <anand.natarajan263@gmail.com>
2020-01-15 19:22:49 +0530

    Showing the project's true colours.

--- weather.py
+++ weather.py
@@ -1,1 +1,11 @@
+import turtle
 
+T = turtle.Screen()
+if MaxTemp == MinTemp:
+	r = 1
+	b = 1
+else:
+	r = (CurrTemp - MinTemp)/(MaxTemp - MinTemp)
+	b = (MaxTemp - CurrTemp)/(MaxTemp - MinTemp)
+T.bgcolor(r, 0, b)
+input()
